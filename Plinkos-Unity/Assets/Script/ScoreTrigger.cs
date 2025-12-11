e0ba4316c085557ddcbaa4668beaecf51da2d061
Gage Parsons <gwparsons2007@gmail.com>
2025-12-11 16:08:42 -0500

    add scores, multipliers, dividers

--- Plinkos-Unity/Assets/Script/ScoreTrigger.cs
+++ Plinkos-Unity/Assets/Script/ScoreTrigger.cs
@@ -6,9 +6,29 @@ public class ScoreTrigger : MonoBehaviour
 
     public ScoreKeeper scoreKeeper;
     public int points = 0;
+    
+    public bool useMultiplier = false;
+    public float multiplier = 1f;
+    
+    public bool useDivider = false;
+    public float Divider = 1f;
     private void OnTriggerEnter2D(Collider2D collider)
     {
+      
+
+        if (useMultiplier)
+        {
+            points = Mathf.RoundToInt(scoreKeeper.Score * multiplier);
+        }
+        if (useDivider)
+        {
+            points = Mathf.RoundToInt(scoreKeeper.Score / Divider);
+        }
+
         scoreKeeper.AddScore(points);
+
+
+
         Destroy(collider.gameObject);
     }
 }
